ec87415ed1ecd1679a9d4a58182c2dd5740dc3e1
gsusgit <fernandezjodar.jesus@gmail.com>
2024-09-08 12:08:02 +0200

    Create Login View and form

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,27 @@
 
 <template>
   <v-card
-    elevation="3"
-    max-width="1200"
-    class="mx-auto"
+      elevation="3"
+      max-width="1200"
+      class="mx-auto"
   >
     <v-layout>
       <v-app-bar
-        color="blue-darken-1"
+          color="blue-darken-1"
       >
-        <template v-slot:append>
-
-        </template>
         <template v-slot:prepend>
-
+          <v-btn
+            :to="{name: 'home'}"
+          >
+            VueFire - Real State
+          </v-btn>
+        </template>
+        <template v-slot:append>
+          <v-btn
+              :to="{name: 'login'}"
+          >
+            Iniciar sesión
+          </v-btn>
         </template>
       </v-app-bar>
       <v-main>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,44 @@
+<script setup>
+
+</script>
+
+<template>
+  <v-card
+      flat
+      max-width="600"
+      class="mx-auto my-10"
+  >
+    <v-card-title
+        class="text-h5 font-weight-bold"
+        tag="h5"
+    >
+      Iniciar sesión
+    </v-card-title>
+    <v-card-subtitle
+        class="text-h6"
+        tag="h6"
+    >
+      Introduce tus credenciales de acceso
+    </v-card-subtitle>
+    <v-form
+        class="mt-5"
+    >
+      <v-text-field
+          label="Email"
+          type="email"
+          bg-color="blue-grey-lighten-5"
+      />
+      <v-text-field
+          label="Password"
+          type="password"
+          bg-color="blue-grey-lighten-5"
+      />
+      <v-btn
+          block
+          color="pink-accent-3"
+      >
+        Iniciar sesión
+      </v-btn>
+    </v-form>
+  </v-card>
+</template>
