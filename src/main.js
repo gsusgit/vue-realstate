af7ad1ef014ae03c873a2929bd925e967e773207
gsusgit <fernandezjodar.jesus@gmail.com>
2024-09-08 13:45:06 +0200

    Install and configure Firebase + Vuefire

--- src/main.js
+++ src/main.js
@@ -6,13 +6,19 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// Vuetify
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+// Firebase
+import { VueFire, VueFireAuth } from 'vuefire'
+import { firebaseApp } from '@/config/firebase.js'
+
 const app = createApp(App)
 
+// Vuetify
 const vuetify = createVuetify({
     components,
     directives,
@@ -22,5 +28,9 @@ const vuetify = createVuetify({
 app.use(createPinia())
 app.use(router)
 app.use(vuetify)
+app.use(VueFire, {
+    firebaseApp,
+    modules: [VueFireAuth()]
+})
 
 app.mount('#app')
