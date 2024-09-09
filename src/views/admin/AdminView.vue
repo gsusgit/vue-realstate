5792b9926cc524ef3e744a321095165ce26ca4fd
gsusgit <fernandezjodar.jesus@gmail.com>
2024-09-09 18:03:39 +0200

    Add some global styles

--- src/views/admin/AdminView.vue
+++ src/views/admin/AdminView.vue
@@ -4,7 +4,7 @@
 
 <template>
   <div class="d-flex w-100 align-center px-4 py-3">
-    <h2 class="text-h4 my-5 font-weight-bold">Admin Panel</h2>
+    <v-card-title class="text-h5 font-weight-bold px-0">Listado propiedades</v-card-title>
     <v-spacer></v-spacer>
     <v-btn :to="{name: 'nueva-propiedad'}" color="teal" class="mt-4">Nueva propiedad</v-btn>
   </div>
