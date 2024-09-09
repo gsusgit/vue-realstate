621c470ade73d5c565d82d47316119c3b660d3aa
gsusgit <fernandezjodar.jesus@gmail.com>
2024-09-09 20:08:55 +0200

    Store form data to Cloud Firestore

--- src/views/admin/EditarPropiedadView.vue
+++ src/views/admin/EditarPropiedadView.vue
@@ -3,7 +3,10 @@
 </script>
 
 <template>
-  <div class="d-flex w-100 align-center py-3">
-    <v-card-title class="text-h4 font-weight-bold px-0">Editar propiedad</v-card-title>
+  <div class="px-4 pt-3">
+    <v-card-title class="text-h5 font-weight-bold px-0">Editar propiedad</v-card-title>
+    <v-card class="mt-3 mb-10 pb-10 px-10">
+
+    </v-card>
   </div>
 </template>
--- src/views/admin/NuevaPropiedadView.vue
+++ src/views/admin/NuevaPropiedadView.vue
@@ -1,68 +1,161 @@
 <script setup>
-  const items = [1, 2, 3, 4, 5]
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { useForm, useField } from 'vee-validate'
+import { collection, addDoc } from 'firebase/firestore'
+import { useFirestore } from 'vuefire'
+import { validationSchema, imageSchema } from '@/validation/propiedadSchema.js'
+
+const items = [1, 2, 3, 4, 5]
+const loading = ref(false)
+const alert = ref({
+  show: false,
+  title: '',
+  type: '',
+})
+
+const { handleSubmit } = useForm({
+  validationSchema: {
+    ...validationSchema,
+    ...imageSchema
+  }
+})
+
+const titulo = useField('titulo')
+const imagen = useField('imagen')
+const precio = useField('precio')
+const habitaciones = useField('habitaciones')
+const aseos = useField('aseos')
+const aparcamientos = useField('aparcamientos')
+const descripcion = useField('descripcion')
+const piscina = useField('piscina', null, {
+  initialValue: false
+})
+
+const db = useFirestore()
+const router = useRouter()  // Mueve esto aquí para que esté disponible en todo el componente
+
+const submit = handleSubmit(async (values) => {
+  loading.value = true
+  const { imagen, ...propiedad } = values
+  try {
+    const docRef = await addDoc(collection(db, 'propiedades'), {
+      ...propiedad
+    })
+    alert.value = {
+      show: true,
+      title: 'Propiedad añadida con éxito.',
+      type: 'success'
+    }
+    setTimeout(() => {
+      alert.value.show = false
+      loading.value = false
+      if (docRef.id) {
+        router.push({ name: 'admin-propiedades' })  // Usa router aquí
+      }
+    }, 1500)
+  } catch (error) {
+    alert.value = {
+      show: true,
+      title: 'Ocurrió un error al intentar añadir propiedad.',
+      type: 'error'
+    }
+    setTimeout(() => {
+      alert.value.show = false
+      loading.value = false
+    }, 1500)
+  }
+})
+
 </script>
 
 <template>
-  <div class="px-4 pt-3">
-    <v-card-title class="text-h5 font-weight-bold px-0">Nueva propiedad</v-card-title>
-    <v-card class="mt-3 mb-10 pb-10 px-10">
+  <div class='px-4 pt-3'>
+    <v-card-title class='text-h5 font-weight-bold px-0'>Nueva propiedad</v-card-title>
+    <v-card class='mt-3 mb-10 pb-10 px-10'>
       <v-form
           @submit.prevent
-          class="mt-10"
+          class='mt-10'
       >
+        <v-alert
+            v-if="alert.show"
+            class="my-5"
+            :title="alert.title"
+            :type="alert.type"
+        ></v-alert>
         <v-text-field
-            label="Título"
-            class="mb-3"
+            label='Título'
+            class='mb-3'
+            v-model='titulo.value.value'
+            :error-messages='titulo.errorMessage.value'
         />
         <v-file-input
-            label="Imagen"
-            prepend-icon="mdi-camera"
-            accept="image/png, image/jpeg"
-            class="mb-3"
+            label='Imagen'
+            prepend-icon='mdi-camera'
+            accept='image/png, image/jpeg'
+            class='mb-3'
+            v-model='imagen.value.value'
+            :error-messages='imagen.errorMessage.value'
         />
         <v-text-field
-            label="Precio"
-            class="mb-3"
+            label='Precio'
+            class='mb-3'
+            v-model='precio.value.value'
+            :error-messages='precio.errorMessage.value'
         />
-        <v-container class="container-3">
+        <v-container class='container-3'>
           <v-row>
-            <v-col cols="12" sm="4">
+            <v-col cols='12' sm='4'>
               <v-select
-                  label="Habitaciones"
-                  class="mb-3"
-                  :items="items"
+                  label='Habitaciones'
+                  class='mb-3'
+                  :items='items'
+                  v-model='habitaciones.value.value'
+                  :error-messages='habitaciones.errorMessage.value'
               />
             </v-col>
-            <v-col cols="12" sm="4">
+            <v-col cols='12' sm='4'>
               <v-select
-                  label="Aseos"
-                  class="mb-3"
-                  :items="items"
+                  label='Aseos'
+                  class='mb-3'
+                  :items='items'
+                  v-model='aseos.value.value'
+                  :error-messages='aseos.errorMessage.value'
               />
             </v-col>
-            <v-col cols="12" sm="4">
+            <v-col cols='12' sm='4'>
               <v-select
-                  label="Plazas garaje"
-                  class="mb-3"
-                  :items="items"
+                  label='Plazas garaje'
+                  class='mb-3'
+                  :items='items'
+                  v-model='aparcamientos.value.value'
+                  :error-messages='aparcamientos.errorMessage.value'
               />
             </v-col>
           </v-row>
         </v-container>
         <v-textarea
-            label="Descripción"
+            label='Descripción'
+            v-model='descripcion.value.value'
+            :error-messages='descripcion.errorMessage.value'
         />
         <v-checkbox
-            label="Piscina"
-            class="mb-3"
+            label='Piscina'
+            class='mb-3'
+            v-model='piscina.value.value'
         />
         <v-btn
-            color="teal-lighten-1"
-            class="text-uppercase"
+            :loading="loading"
+            :disabled="loading"
+            color='teal-lighten-1'
+            class='text-uppercase'
+            @click='submit'
         >
-          Añadir propiedad
+          <v-icon v-if="loading">mdi-loading</v-icon>
+          <span>{{ loading ? 'Añadiendo...' : 'Añadir propiedad' }}</span>
         </v-btn>
       </v-form>
     </v-card>
   </div>
 </template>
+
