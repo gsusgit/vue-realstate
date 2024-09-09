e58c5137c303a45ae730b50d37e97170b0e1d55c
gsusgit <fernandezjodar.jesus@gmail.com>
2024-09-09 11:35:50 +0200

    Reformat login schema helper

--- src/validation/loginSchema.js
+++ src/validation/loginSchema.js
@@ -1,14 +1,14 @@
 export const loginSchema = {
     email(value) {
         if (!value) {
-            return 'Este campo es obligatorio';
+            return 'Este campo es obligatorio'
         }
         // si es un email válido
-        const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+        const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
         if (!regex.test(value)) {
-            return 'Email no válido';
+            return 'Email no válido'
         }
-        return true;
+        return true
     },
     password(value) {
         if (value) return true
